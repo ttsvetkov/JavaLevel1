22797fed0798f32211c9c0b1791d66a65f7f4e23
Tsvetkov Anatoliy <ttsvetkov>
2021-05-10 13:30:24 +0300

    исправил условие цикла for (PERS-1)
    Signed-off-by: Tsvetkov Anatoliy <ttsvetkov>

--- src/ru/geekbrains/java1/lesson5/HomeWork5.java
+++ src/ru/geekbrains/java1/lesson5/HomeWork5.java
@@ -9,14 +9,14 @@ protected static final int PERS = 5;
 
         Worker[] persArray = new Worker[PERS];
 
-        for (int i = 0; i <= PERS-1; i++){
+        for (int i = 0; i < PERS; i++){
             persArray[i] = new Worker();
             System.out.println(persArray[i]);
         }
 
         System.out.println("\n ----------------Сотрудники страше сорока------------");
 
-        for (int i = 0; i <= PERS-1; i++){
+        for (int i = 0; i < PERS; i++){
             if (persArray[i].getAge() > Worker.AGE_LIMIT){
                 persArray[i].info();
             }
